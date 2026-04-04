d73d0a61d74e620b874ac01ba0e43131924d4cef
Delmat237 <azangueleonel9@gmail.com>
2026-04-04 20:53:12 +0100

    CORE

--- src/lib/core/OpenAPI.ts
+++ src/lib/core/OpenAPI.ts
@@ -21,8 +21,8 @@ export type OpenAPIConfig = {
 
 export const OpenAPI: OpenAPIConfig = {
 
-     BASE: 'https://xccm1-backend-eews.onrender.com',
-    //BASE: 'https://xccm1-backend-eews.onrender.com',
+     //BASE: 'https://xccm1-backend-eews.onrender.com',
+    BASE: 'http://localhost:8082',
 
     VERSION: '1.0.0',
     WITH_CREDENTIALS: false,
